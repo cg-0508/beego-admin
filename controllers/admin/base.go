54ee29379ccff30fee6730982fcf618068df3f69
cg-0508 <cg-0508@outlook.com>
2019-01-22 14:42:52 +0800

    orm

--- controllers/admin/base.go
+++ controllers/admin/base.go
@@ -10,8 +10,7 @@ import (
 
 func init() {
 	// set default database
-	//orm.RegisterDataBase("default", "mysql", "root:@tcp(127.0.0.1:3306)/beego-blog?charset=utf8", 30)
-	orm.RegisterDataBase("default", "mysql", "root:Gmy19970812@tcp(123.207.66.141:3306)/beego-blog?charset=utf8", 30)
+	orm.RegisterDataBase("default", "mysql", "root:@tcp(127.0.0.1:3306)/beego-blog?charset=utf8", 30)
 
 	orm.RegisterModel(new(models.User))
 
